dff5838cd196c20dfb9e71f7afdddde42da322ad
lido <62131409+listdo@users.noreply.github.com>
2022-01-23 17:23:53 +0100

    Add files via upload

--- EncryptionExamples/Program.cs
+++ EncryptionExamples/Program.cs
@@ -8,6 +8,13 @@ namespace EncryptionExamples
     {
         static void Main(string[] args)
         {
+            HashDemo();
+
+            //AESDemo();
+        }
+
+        public static void AESDemo()
+        {
             string original = "Here is some data to encrypt!";
 
             // Create a new instance of the Aes
@@ -25,6 +32,7 @@ namespace EncryptionExamples
                 Console.WriteLine("Round Trip: {0}", roundtrip);
             }
         }
+
         static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
         {
             // Check arguments.
@@ -64,7 +72,6 @@ namespace EncryptionExamples
             // Return the encrypted bytes from the memory stream.
             return encrypted;
         }
-
         static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
         {
             // Check arguments.
@@ -106,6 +113,63 @@ namespace EncryptionExamples
             }
 
             return plaintext;
+        }
+    
+        static void HashDemo()
+        {
+            string directory = @"C:\temp";
+            if (Directory.Exists(directory))
+            {
+                // Create a DirectoryInfo object representing the specified directory.
+                var dir = new DirectoryInfo(directory);
+                // Get the FileInfo objects for every file in the directory.
+                FileInfo[] files = dir.GetFiles();
+                // Initialize a SHA256 hash object.
+                using (SHA256 mySHA256 = SHA256.Create())
+                {
+                    // Compute and print the hash values for each file in directory.
+                    foreach (FileInfo fInfo in files)
+                    {
+                        using (FileStream fileStream = fInfo.Open(FileMode.Open))
+                        {
+                            try
+                            {
+                                // Create a fileStream for the file.
+                                // Be sure it's positioned to the beginning of the stream.
+                                fileStream.Position = 0;
+                                // Compute the hash of the fileStream.
+                                byte[] hashValue = mySHA256.ComputeHash(fileStream);
+                                // Write the name and hash value of the file to the console.
+                                Console.Write($"{fInfo.Name}: ");
+                                PrintByteArray(hashValue);
+                            }
+                            catch (IOException e)
+                            {
+                                Console.WriteLine($"I/O Exception: {e.Message}");
+                            }
+                            catch (UnauthorizedAccessException e)
+                            {
+                                Console.WriteLine($"Access Exception: {e.Message}");
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("The directory specified could not be found.");
+            }
+        }
+
+        // Display the byte array in a readable format.
+        public static void PrintByteArray(byte[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]:X2}");
+                if ((i % 4) == 3) Console.Write(" ");
+            }
+            Console.WriteLine();
         }
     }
 }
